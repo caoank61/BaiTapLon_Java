c459584649e0a30143e6f3887f12cb3264345c67
Dungpi <phamdung.080602@gmail.com>
2022-04-13 08:58:22 +0700

    dieu kien

--- BaiTapLon_Java/src/DungPham/Hoc_Sinh.java
+++ BaiTapLon_Java/src/DungPham/Hoc_Sinh.java
@@ -130,30 +130,119 @@ public class Hoc_Sinh {
 		Lop = sc.nextLine();
 		System.out.print("Nhap Dia Chi (Thanh Pho): ");
 		Dia_Chi = sc.nextLine();
-		System.out.print("Nhap Ngay Sinh: ");
-		Ngay = sc.nextInt();
-		System.out.print("Nhap Thang Sinh: ");
-		Thang = sc.nextInt();
-		System.out.print("Nhap Nam Sinh: ");
-		Nam = sc.nextInt();
+		do {
+			System.out.print("Nhap Ngay Sinh: ");
+			Ngay = sc.nextInt();
+			System.out.print("Nhap Thang Sinh: ");
+			Thang = sc.nextInt();
+				if(KiemTra() == false){
+		            System.out.println("Ngay Thang Khong Hop Le !! Nhap Lai Ngay Thang!!");
+		            System.out.print("Nhap Ngay Sinh: ");
+					Ngay = sc.nextInt();
+					System.out.print("Nhap Thang Sinh: ");
+					Thang = sc.nextInt();
+				}
+		}while(KiemTra()==false);
+		do {
+			System.out.print("Nhap Nam Sinh: ");
+			Nam = sc.nextInt();
+				if(Nam > 2006 || Nam < 2004){
+			        System.out.println("Nhap lai nam sinh!!");
+			        System.out.print("Nhap Nam Sinh: ");
+					Nam = sc.nextInt();
+			    }
+		    }while(Nam > 2006 || Nam < 2004);
 		
 		// Nhập thông tin điểm các môn của học sinh
 		System.out.println("Nhap Diem Hoc Sinh!!!");
-		System.out.print("Nhap Diem Toan: ");
-		Diem_Toan = sc.nextFloat();
-		System.out.print("Nhap Diem Ly: ");
-		Diem_Ly = sc.nextFloat();
-		System.out.print("Nhap Diem Hoa: ");
-		Diem_Hoa = sc.nextFloat();
-		System.out.print("Nhap Diem Van: ");
-		Diem_Van = sc.nextFloat();
-		System.out.print("Nhap Diem Su: ");
-		Diem_Su = sc.nextFloat();
-		System.out.print("Nhap Diem Dia: ");
-		Diem_Dia = sc.nextFloat();
+		do {
+			System.out.print("Nhap Diem Toan: ");
+			Diem_Toan = sc.nextFloat();
+			if (Diem_Toan < 0 || Diem_Toan > 10) {
+				System.out.println("Nhap Lai Diem Toan:");
+				Diem_Toan = sc.nextFloat();
+			}
+		}while (Diem_Toan < 0 || Diem_Toan > 10);
+		do {
+			System.out.print("Nhap Diem Ly: ");
+			Diem_Ly = sc.nextFloat();
+			if (Diem_Ly < 0 || Diem_Ly > 10) {
+				System.out.println("Nhap Lai Diem Ly:");
+				Diem_Ly = sc.nextFloat();
+			}
+		}while (Diem_Ly < 0 || Diem_Ly > 10);
+		do {
+			System.out.print("Nhap Diem Hoa: ");
+			Diem_Hoa = sc.nextFloat();
+			if (Diem_Hoa < 0 || Diem_Hoa > 10) {
+				System.out.println("Nhap Lai Diem Hoa:");
+				Diem_Hoa = sc.nextFloat();
+			}
+		}while (Diem_Hoa < 0 || Diem_Hoa > 10);
+		do {
+			System.out.print("Nhap Diem Van: ");
+			Diem_Van = sc.nextFloat();
+			if (Diem_Van < 0 || Diem_Van > 10) {
+				System.out.println("Nhap Lai Diem Van:");
+				Diem_Van = sc.nextFloat();
+			}
+		}while (Diem_Van < 0 || Diem_Van > 10);
+		do {
+			System.out.print("Nhap Diem Su: ");
+			Diem_Su = sc.nextFloat();
+			if (Diem_Su < 0 || Diem_Su > 10) {
+				System.out.println("Nhap Lai Diem Su:");
+				Diem_Su = sc.nextFloat();
+			}
+		}while (Diem_Su < 0 || Diem_Su > 10);
+		do {
+			System.out.print("Nhap Diem Dia: ");
+			Diem_Dia = sc.nextFloat();
+			if (Diem_Dia < 0 || Diem_Dia > 10) {
+				System.out.println("Nhap Lai Diem Dia:");
+				Diem_Dia = sc.nextFloat();
+			}
+		}while (Diem_Dia < 0 || Diem_Dia > 10);
 	}
 
 	public void Xuat_Thong_Tin_Hoc_Sinh () {
 		System.out.println("MSHS: " + MSHS +" || Ten: "+ HoTen +" || Lop: "+ Lop + " || Dia chi: "+ Dia_Chi + " || Ngay Thang Nam Sinh: "+Ngay +"/"+Thang+ "/"+Nam+" || Tuoi: "+Tinh_Tuoi()+" || Toan: "+Diem_Toan+" || Ly: "+Diem_Ly+" || Hoa: "+Diem_Hoa+" || Van: "+Diem_Van+" || Su: "+Diem_Su+" || Dia: "+Diem_Dia + " || Diem Trung Binh: " + Tinh_DiemTB());
 	}
+	public boolean KiemTra(){
+		    //kiem tra ngay thang
+		    boolean KiemTra = true;
+		    if (Thang < 0 || Thang > 12) {
+		        KiemTra = false;
+		    }
+		    else {
+		        switch (Thang)
+		        {
+		        case 1:
+		        case 3:
+		        case 5:
+		        case 7:
+		        case 8:
+		        case 10:
+		        case 12:
+		            if (Ngay < 1 || Ngay > 31) {
+		                KiemTra = false;
+		            }
+		            break;
+		        case 4:
+		        case 6:
+		        case 9:
+		        case 11:
+		            if (Ngay < 1 || Ngay > 30) {
+		                KiemTra = false;
+		            }
+		            break;
+		        case 2:
+		            if (Nam % 4 == 0 && Ngay < 1 || Ngay > 29) {
+		                KiemTra = false;
+		            }
+		            break;
+		        }
+		    }
+		    return KiemTra;
+		}
 }
