32218f95489bd1a2e8838fca6e1dbc2ef6770ef8
Dungpi <phamdung.080602@gmail.com>
2022-04-28 08:37:02 +0700

    fix chuc nang

--- BaiTapLon_Java/src/DungPham/ChucNang.java
+++ BaiTapLon_Java/src/DungPham/ChucNang.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class ChucNang {
 	Hoc_Sinhs sds = new Hoc_Sinhs();// sds: Students
-	Hoc_Sinh_Chuyen hsc = new Hoc_Sinh_Chuyen(); //hsc : Hoc Sinh Chuyen
 	HS_Chuyens hscs = new HS_Chuyens ();
 	public void Them_HocSinh_Chuyen () {
 		sds.Nhap_Nhieu_Hs();
@@ -18,10 +17,11 @@ public class ChucNang {
 		machuyen = sc.nextLine();
 		for (int i = 0;i < sds.size();i++) {
 			if (machuyen.equals(((Hoc_Sinh) sds.elementAt(i)).getMSHS())) {
+				Hoc_Sinh_Chuyen hsc = new Hoc_Sinh_Chuyen();
 				hsc.setHoTen(((Hoc_Sinh) sds.elementAt(i)).getHoTen());
 				hsc.setLop(((Hoc_Sinh) sds.elementAt(i)).getLop());
 				hsc.setMSHS(((Hoc_Sinh) sds.elementAt(i)).getMSHS());
-				hsc.setDia_Chi(((Hoc_Sinh) sds.elementAt(i)).getDia_Chi());//
+				hsc.setDia_Chi(((Hoc_Sinh) sds.elementAt(i)).getDia_Chi());
 				hsc.setNgay(((Hoc_Sinh) sds.elementAt(i)).getNgay());
 				hsc.setThang(((Hoc_Sinh) sds.elementAt(i)).getThang());
 				hsc.setNam(((Hoc_Sinh) sds.elementAt(i)).getNam());
@@ -34,7 +34,6 @@ public class ChucNang {
 				hsc.Nhap_Hoc_Sinh_Chuyen();
 				count ++;
 				hscs.add(hsc);
-				break;
 			}
 		}
 		if (count == 0) {
@@ -51,5 +50,4 @@ public class ChucNang {
 	public void Xuat_HS_Chuyens() {
 		hscs.Xuat_HS_Chuyen();
 	}
-	public void a() {}
 }
